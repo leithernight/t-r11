3f8d182ef09557707e0414c0c154e8022cc10741
leithernight <rassokhina.o@mail.ru>
2024-04-09 00:09:13 +0300

    text

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductAlertsComponent,
+    ProductListComponent,
+    TopBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
